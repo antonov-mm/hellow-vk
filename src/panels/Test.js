58d191153c794943a25ddbec7b32100650dbe147
Максим <57525396+kaban4ik163@users.noreply.github.com>
2020-01-14 01:19:57 +0400

    Add files via upload

---
+++ src/panels/Test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { platform, IOS } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+const osName = platform();
+
+const Test = props => (
+	<Panel id={props.id}>
+		<PanelHeader
+			left={<HeaderButton onClick={props.go} data-to="home">
+				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+			</HeaderButton>}
+		>
+			Я персик
+		</PanelHeader>
+	</Panel>
+);
+
+Test.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
+
+export default Test;
